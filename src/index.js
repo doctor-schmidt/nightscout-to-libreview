d8081ac1dfdedc744adec5159583784523b3442b
doctor-schmidt <doctor-schmidt@bluewin.ch>
2024-06-25 11:47:59 +0200

    default 92 days = 3 months

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ prompt.get([{
   description: 'enter number of days you want to transfer to libreview',
   required: true,
   type: 'number',
-  default: 100
+  default: 92
 }, {
   name: 'libreResetDevice',
   description: 'if you have problems with your transfer, recreate the device id',
